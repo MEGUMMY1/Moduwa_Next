ed2261b5d66135faef6afc067ac8324b937d190d
alex <alex@naver.com>
2023-12-26 17:59:32 +0900

    Alex

--- src/app/(Logined)/news/page.tsx
+++ src/app/(Logined)/news/page.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 
-import FeedElement from "./_components/feedElement";
-import feedData from "../../../../public/feeddata.json"
-import FeedTop from "./_components/feedTop";
-
 const Page = () => {
   return (
     <>
-      <FeedTop />
-      {feedData.map((feed) => (
-        <FeedElement key={feed.id} feed={feed} />
-      ))}
+    
     </>
   );
 };
