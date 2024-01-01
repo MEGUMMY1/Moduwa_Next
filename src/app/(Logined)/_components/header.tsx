94b1dadafdfeae30eb59b0a34e8a854a4e927f41
DavidLeeeee <david431385@gmail.com>
2024-01-02 02:35:41 +0900

    0101/Prisma-Login

--- src/app/(Logined)/_components/header.tsx
+++ src/app/(Logined)/_components/header.tsx
@@ -1,9 +1,20 @@
 import styles from "./header.module.css";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <div className={styles.headerContainer}>
-      <div className={styles.logo} />
+      <Image
+        src={"/image/logo_modu3.svg"} // 기본 이미지 설정}
+        alt={"Moduwa"}
+        width={92}
+        height={40}
+        style={{
+          objectFit: "contain",
+        }}
+        //layout="intrinsic" // 또는 'fixed', 'intrinsic' 등에 따라 설정
+      />
+      {/* <div className={styles.logo} /> */}
     </div>
   );
 };
